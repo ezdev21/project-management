3e78d2de5d2c84fbfcb0100ada7f2f841b880b2b
ezra <ezrafikadu02@gmail.com>
2023-03-27 22:47:43 +0300

    client router setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import './index.css'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          
+        </nav>
+      </header>
+      <main>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>  
   )
 }
 
